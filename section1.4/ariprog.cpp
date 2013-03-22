dab7fa2fd489e38ef6249acf0056fbed0fba6267
Simon Vocella <voxsim@gmail.com>
2013-03-22 19:48:54 +0100

    add PROBLEMS file and complexity for section 1.4

--- section1.4/ariprog.cpp
+++ section1.4/ariprog.cpp
@@ -4,25 +4,14 @@ NUMBER: 3
 ID: voxsim2
 PROG: ariprog
 LANG: C++
+COMPLEXITY: O(m^2 + 2*(m^2/n)*n) = O(m^2 + 2*m^2) = O(3*m^2)
 */
-
 /*
-Text: 
-An arithmetic progression is a sequence of the form a, a+b,
-a+2b, ..., a+nb where n=0,1,2,3,... . For this problem, a is a
-non-negative integer and b is a positive integer.
-Write a program that finds all arithmetic progressions of length
-n in the set S of bisquares. The set of bisquares is defined as the
-set of all integers of the form p<sup>2</sup> + q<sup>2</sup> (where
-p and q are non-negative integers).
-
 Solution:
 First we create the set of bisquares.
 Second we limit b: a+n*b <= 2*m*m  ==> b <= (2*m*m-a)/n
 In the worst case a=0 so b <= (2*m*m)/n
 So we cycle for the possible values of b and for the possible values of a (the bisquares)
-
-Complexity: O(m^2 + 2*(m^2/n)*n) = O(m^2 + 2*m^2) = O(3*m^2)
 */
 
 #include <iostream>

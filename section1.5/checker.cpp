02b0ba5d7b188e466f685a62cd182f9f7c2816a2
Simon Vocella <voxsim@gmail.com>
2013-03-22 19:52:25 +0100

    add PROBLEMS file for section 1.5

--- section1.5/checker.cpp
+++ section1.5/checker.cpp
@@ -4,38 +4,9 @@ NUMBER: 4
 ID: voxsim2
 PROG: checker
 LANG: C++
-
-TEXT:
-
-
-Checker Challenge
-
-Examine the 6x6 checkerboard below and note that the six checkers are arranged on the board so that one and only one is placed in each row and each column, and there is never more than one in any diagonal. (Diagonals run from southeast to northwest and southwest to northeast and include all diagonals, not just the major two.)
-
-          Column
-    1   2   3   4   5   6
-  -------------------------
-1 |   | O |   |   |   |   |
-  -------------------------
-2 |   |   |   | O |   |   |
-  -------------------------
-3 |   |   |   |   |   | O |
-  -------------------------
-4 | O |   |   |   |   |   |
-  -------------------------
-5 |   |   | O |   |   |   |
-  -------------------------
-6 |   |   |   |   | O |   |
-  -------------------------
-
-The solution shown above is described by the sequence 2 4 6 1 3 5, which gives the column positions of the checkers for each row from 1 to 6:
-ROW 1   2   3   4   5   6
-COLUMN  2   4   6   1   3   5
-
-This is one solution to the checker challenge. Write a program that finds all unique solution sequences to the Checker Challenge (with ever growing values of N). Print the solutions using the column notation described above. Print the the first three solutions in numerical order, as if the checker positions form the digits of a large number, and then a line with the total number of solutions. 
-
+*/
+/*
 Solution: DFS with memoization, check the status in col, diagonal left and digonal right.
-
 Complexity: ??
 */
 

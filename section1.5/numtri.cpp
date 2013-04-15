bb22088fae2a07bef601a7458806146837461059
Simon Vocella <voxsim@gmail.com>
2013-04-15 15:21:34 +0200

    add numtri problem of section1.5

--- section1.5/numtri.cpp
+++ section1.5/numtri.cpp
@@ -10,49 +10,31 @@ LANG: C++
 
 using namespace std;
 
-/*
- * Albero binario completo:
- * radice i e figli in 2i e 2i+1 (partendo da 1)
- * Altezza massima R: 1000
- * numero massimo 500*1001 = 500500
-*/
+unsigned int R;
+unsigned int values[1000][1000];
 
-/*
- * radice i e figli 2i e 2i+1
- *
- * 1 -> 2,3
- * 2 -> 4,5
- * 3 -> 6,7
- * 4 -> 8,9
- * 5 -> 10,11
- * 6 -> 12,13
- * 7 -> 14,15
- *
- * radice i e figli 2i-l e 2i+1-l (l indice di quanto sei a destra, inizia da 0)
- *
- * i=1, h=0, l=0 -> 2,3
- * i=2, h=0, l=0 -> 4,5
- * i=3, h=0, l=1 -> 5,6
- * i=4, h=1, l=0 -> 7,8
- * i=5, h=1, l=1 -> 8,9
- * i=6, h=1, l=2 -> 9,10
- * i=7, h=2, l=1 -> 11,12
- *
- * */
-
-unsigned int n;
-unsigned int values[500500];
+unsigned int max(unsigned int a, unsigned int b) {
+    return a > b ? a : b;
+}
 
 int main() {
     ofstream fout ("numtri.out");
     ifstream fin ("numtri.in");
 
-    fin >> n;
+    fin >> R;
 
-    for(unsigned int i=0; i<((n*(n+1))/2); i++) {
-        fin >> values[i];
-        cout << "pos: " << i+1 << " => val: " << values[i] << "\n";
+    for(unsigned int i=0; i<R; i++) {
+        for(unsigned int j=0; j<i+1; j++) {
+            fin >> values[i][j];
+        }
+    }
+        
+    for(int i=R-2; i>-1; i--) {
+        for(unsigned int j=0; j<i+1; j++) {
+            values[i][j] += max(values[i+1][j], values[i+1][j+1]);
+        }
     }
 
+    fout << values[0][0] << endl;
     return 0;
 }
